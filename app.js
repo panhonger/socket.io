c7289231baf7a4616cedcb74660d4664f86d032c
panhongfan <1581663693@qq.com>
2019-01-22 16:44:45 +0800

    socket.io

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const users = [];
+
+let kit={
+    isHaveUser(user){
+        var flag=false;
+        users.forEach(function (item) {
+            if(item.name==user.name){
+                flag=true;
+            }
+        })
+        return flag;
+    },
+    delUser(id){
+        users.forEach(function (item,index) {
+            if(item.id==id){
+                users.splice(index, 1);
+            }
+        })
+    }
+}
+
+app.use('/', express.static(__dirname + '/static'));
+
+io.sockets.on('connection', function(socket) {
+    //创建用户链接
+    socket.on('login', function(user) {
+        if (kit.isHaveUser(user)) {
+            console.log("登录失败！",user)
+            socket.emit('loginFail',"登录失败,昵称已存在!");
+        } else {
+            socket.user = user;
+            user.id=socket.id;
+            console.log("登录成功！",user)
+            socket.emit('loginSuccess',user,users);
+            users.push(user)
+            socket.broadcast.emit('system1', user,'join');
+        };
+    });
+    //用户注销链接
+    socket.on('disconnect', function() {
+        if (socket.user!= null) {
+            kit.delUser(socket.id);
+            console.log("用户退出！",socket.user)
+            socket.broadcast.emit('system', socket.user, 'logout');
+        }
+    });
+    //群发消息
+    socket.on('groupMessage', function(msg,from){
+        if(!socket.user){
+            from.id=socket.id;
+            socket.user=from;
+            users.push(from);
+            socket.broadcast.emit('system', from,'join');
+            socket.emit('loginSuccess',from,[]);
+        }
+        socket.broadcast.emit('groupMessage', socket.user, msg);
+    });
+    //发送私信
+    socket.on('message', function(id,msg,from) {
+        if(!socket.user){
+            from.id=socket.id;
+            socket.user=from;
+            users.push(from);
+            socket.broadcast.emit('system', from,'join');
+            socket.emit('loginSuccess',from,[]);
+        }
+        socket.broadcast.to(id).emit('message',socket.user,msg);
+    });
+});
+
+server.listen(14006,function () {
+    console.log("服务器已启动在：14006端口","http://localhost:14006")
+});
+console.log(__dirname)
